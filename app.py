0224fd374749a5133c3b2432c656bc49379da2da
Likhit Bhogadi <likhitbhogadi@gmail.com>
2024-03-24 14:46:52 +0530

    new

--- app.py
+++ app.py
@@ -78,9 +78,9 @@ db = SQLAlchemy(app)
 # Replace placeholders with actual values
 
 engine = create_engine(
-    "cockroachdb://likhitbhogadi:1ge-6vxKIC_SVgYBbMf0Wg@crawly-ewe-9007.8nk.gcp-asia-southeast1.cockroachlabs.cloud:26257/issproject&sslrootcert=./root.crt",
-    connect_args={"sslmode": "require"}
+    "cockroachdb://likhitbhogadi:1ge-6vxKIC_SVgYBbMf0Wg@crawly-ewe-9007.8nk.gcp-asia-southeast1.cockroachlabs.cloud:26257/issproject?sslmode=require&sslrootcert=./root.crt",
 )
+
 # 6vxKIC_SVgYBbMf0Wg
 # Establish connection and execute SQL query
 # engine = create_engine(os.environ["DATABASE_URL"])
